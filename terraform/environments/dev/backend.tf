dfed11c0a885960313ccfecddde696684e05b859
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-07-09 20:09:50 +0530

    backend.tf

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket       = "shubhsjadhav95-terraform-s3-1"
-    key          = "backend-locking"
-    region       = "us-east-1"
-    use_lockfile = true
+    bucket  = "shubhsjadhav95-terraform-s3-1"
+    key     = "backend-locking"
+    region  = "us-east-1"
+    encrypt = true
   }
 }
