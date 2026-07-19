4555d48d75cf0c8875ba0ad9918a24ef2b364482
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-07-19 19:02:27 +0530

    namespace name changed

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -15,7 +15,7 @@ module "secrets_csi" {
   cluster_name      = module.eks.cluster_name
   oidc_provider_arn = module.eks.oidc_provider_arn
   oidc_provider_url = module.eks.oidc_provider_url
-  app_namespace     = "neocare"
+  app_namespace     = "one8pulse"
 }
 
 # ── 1. VPC ──
