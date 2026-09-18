0389cc6c00a1b17d9ae8d2d4fa98654b9cefc0de
shubhsJadhav95 <shubhamjadhav952004@gmail.com>
2026-09-18 17:30:26 +0530

    terraform backend bucket name is updated

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "shubhsjadhav95-terraform-s3-1"
+    bucket       = "samlondhe95-terraform-s3-1"
     key          = "backend-locking"
     region       = "us-east-1"
     use_lockfile = true
